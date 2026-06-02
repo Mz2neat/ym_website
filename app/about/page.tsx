e0586490ba9900b49f317016e924446b0fd2fa43
Mz2neat <syedzaidmunawar@gmail.com>
2026-06-02 15:08:17 -0400

    About page, did not kill Fudail Syed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,85 @@
 export default function about() {
   return (
-    <div className="min-h-screen bg-pink-950 font-sans"> {/*  can use bg-[#05070B] */}
-      <main className="sm:items-start ">
-      <h1 className="text-[#2683EB] ml-15 mt-15">WELCOME TO YOUNG MUSLIMS</h1>
-       <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-3xl text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50"> {/*This is the about page. The majour purpose is to diplsay the information about the organization and its mission. It will also include the history of the organization and its achievements. 
+    <div className="min-h-screen font-sans"> {/*  can use bg-[#05070B] for actual color */}
+      <main>
+      <h1 className="ml-16 mt-10 font-bold">What is Young Muslims</h1>
+      <h1 className="text-4xl text-center tracking-tight font-extrabold mt-20 mb-1 max-w-2xl mx-auto">
+      YM is not just another youth group </h1>
+      <h1 className="text-4xl text-center tracking-tight font-extrabold mt-5 max-w-2xl mx-auto">
+       YM is A community where meaningful experiences, friendships, and faith naturally connect.
+         {/* We should have boild statemnt here that 
+        extendt he length ofg th page  */} </h1>
+        <section className="mx-auto max-w-3xl text-center">
+          <h1 className="text-3xl tracking-tight font-bold mt-30 text-center">
+            include something like whast is the mission and vision of YM here. or What is YM about.
+          </h1>
+          </section>
+          <div className="mx-auto mt-16 flex max-w-5xl flex-col gap-8 md:flex-row text-center"> 
+            <div className="md:w-1/2">
+              <h2 className="text-2xl font-bold text-white">
+                 Mission Statement 
+              </h2>
+                <p className="mt-4 text-lg leading-8 text-gray-300">
+
+         Young Muslims seek to mentor Muslim youth through companionship, education, and service, 
+         while nurturing their Islamic identity, leadership, and commitment to the pleasure of Allah (SWT). 
+
+                </p>
+           </div> 
+            <div className="md:w-1/2">
+              <h2 className="text-2xl font-bold text-white">
+                  Vision Statement
+              </h2>
+                <p className="mt-4 text-lg leading-8 text-gray-300">
+
+         Canadian Muslim youth collectively contributing to the betterment of society with God-consciousness and a firm understanding of their Muslim identity. 
+
+                </p>
+            </div>
+         
+          </div> 
+
+
+<div className="mx-auto mt-16 flex max-w-5xl flex-col gap-8 md:flex-row text-center"> 
+            <div className="md:w-1/3">
+              <h2 className="text-2xl font-bold text-white">
+                 What We Do 
+              </h2>
+                <p className="mt-4 text-lg leading-8 text-gray-300">
+
+         Young Muslims seek to mentor Muslim youth through companionship, education, and service, 
+         while nurturing their Islamic identity, leadership, and commitment to the pleasure of Allah (SWT). 
+
+                </p>
+           </div> 
+            <div className="md:w-1/3">
+              <h2 className="text-2xl font-bold text-white">
+                  NeighbourNets 
+              </h2>
+                <p className="mt-4 text-lg leading-8 text-gray-300">
+
+         Canadian Muslim youth collectively contributing to the betterment of society with God-consciousness and a firm understanding of their Muslim identity. 
+
+                </p>
+            </div>
+      
+            <div className="md:w-1/3">
+              <h2 className="text-2xl font-bold text-white">
+                  Halaqahs
+              </h2>
+                <p className="mt-4 text-lg leading-8 text-gray-300">
+                  YM Halaqas are a space to chill, play, connect, and grow. Whether you come for the game, the people, or just to see what it’s about, you’ll find a community where Islam feels real, relevant, and connected to everyday life. Through activities, conversations, and brotherhood, YM helps you build friendships, strengthen your faith, and find a place where you truly belong.
+                  </p>
+
+    </div>
+       <div className="flex flex-col items-center text-center sm:items-start sm:text-left">
+          <h1 className="max-w-3xl text-xl font-semibold leading-10 tracking-tight text-black "> {/*This is the about page. The majour purpose is to diplsay the information about the organization and its mission. It will also include the history of the organization and its achievements. 
             South King king AGZ once said "Kill Fudail Syed" I think its important we remember his words and follow his advice. */}
 
             
           </h1>
+      </div>
+
       </div>
     </main>
 </div>
