0bad7b27e384989b64c23dfb9dead36953b2221d
Mz2neat <syedzaidmunawar@gmail.com>
2026-06-03 16:10:26 -0400

    made changes to about layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,12 +14,12 @@ export default function about() {
             include something like whast is the mission and vision of YM here. or What is YM about.
           </h1>
           </section>
-          <div className="mx-auto mt-16 flex max-w-[99%] flex-col gap-8 md:flex-row text-center"> 
-            <div className="md:w-1/2">
-              <h2 className="text-2xl font-bold text-white">
+          <div className="mx-auto mt-16 flex max-w-[99%] flex-col gap-8 md:flex-row text-center"> {/*This code segment (lines 17-41) covers the text for the mission and vission statements The width is precentage to avoid scaling issues on other devices */}
+            <div className="md:w-1/2"> {/* makes text boxes 1/2 width of set width from above (99% of screen width)*/}
+              <h2 className="text-2xl font-bold text-white"> {/* for missionsatament heading*/}
                  Mission Statement 
               </h2>
-                <p className="mt-4 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto">
+                <p className="mt-4 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto"> {/* for mission statement text. max width is 70% of the 99% from above to avoid scaling issues on other devices and to avoid have the text going all the way to the edges*/}  
 
          Young Muslims seek to mentor Muslim youth through companionship, education, and service, 
          while nurturing their Islamic identity, leadership, and commitment to the pleasure of Allah (SWT). 
@@ -30,7 +30,7 @@ export default function about() {
               <h2 className="text-2xl font-bold text-white">
                   Vision Statement
               </h2>
-                <p className="mt-4 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto">
+                <p className="mt-4 mb-30 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto">
 
          Canadian Muslim youth collectively contributing to the betterment of society with God-consciousness and a firm understanding of their Muslim identity. 
 
@@ -39,37 +39,57 @@ export default function about() {
          
           </div> 
 
+{/*<section className="mx-auto max-w-3xl text-center">
+          <h1 className="text-3xl tracking-tight font-bold mt-30 text-center">
+            What We Do
+          </h1>
+          </section>*/} {/*this is the what we do text/headding*/}
+        
+          <div className="mt-8 mx-auto max-w-[90%] text-center"> {/* */}
+              <h2 className=" mx-auto text-2xl font-bold text-white"> {/* for halaqah heading*/}
+                 What is YM a Halaqah
+              </h2>
+                <p className="mt-4 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto "> {/* */}
+         YM Halaqas are a space to chill, play, connect, and grow. Whether you come for the game, the people, or just to see what it’s about, you’ll find a community where Islam feels real, relevant, and connected to everyday life. Through activities, conversations, and brotherhood, YM helps you build friendships, strengthen your faith, and find a place where you truly belong. 
 
+                </p>
+           </div> 
+           <div className="mt-4 mx-auto max-w-[90%] text-center"> {/*what does ym a halqaah include */}
+            <h2 className=" mt-10 mx-auto text-xl font-bold text-white"> {/* */}
+                What does a YM Halaqah Include?
+              </h2>
+                <p className="mt-2 text-lg leading-8 text-gray-300 max-w-[50%] mx-auto"> {/* */}
+         Every YM gathering begins with a relatable Islamic reminder, a short, interactive talk led by a YM member or guest speaker, followed by an engaging activity like basketball, soccer, or gaming. It’s about learning the Deen while building brotherhood in a space where you truly belong.
+                </p>
+           </div>
 <div className="mx-auto mt-16 flex max-w-5xl flex-col gap-8 md:flex-row text-center"> 
             <div className="md:w-1/3">
               <h2 className="text-2xl font-bold text-white">
-                 What We Do 
+                 Sports/Activites 
               </h2>
-                <p className="mt-4 text-lg leading-8 text-gray-300">
+                <p className="mt-4 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto">
 
-         Young Muslims seek to mentor Muslim youth through companionship, education, and service, 
-         while nurturing their Islamic identity, leadership, and commitment to the pleasure of Allah (SWT). 
+         something about gaming or ball 
 
                 </p>
            </div> 
             <div className="md:w-1/3">
               <h2 className="text-2xl font-bold text-white">
-                  NeighbourNets 
+                  Khaterah 
               </h2>
                 <p className="mt-4 text-lg leading-8 text-gray-300">
 
-         Canadian Muslim youth collectively contributing to the betterment of society with God-consciousness and a firm understanding of their Muslim identity. 
-
+         Soemthing about inclduign Islamic reminder or talk at the beginning of the halaqah
                 </p>
             </div>
       
             <div className="md:w-1/3">
               <h2 className="text-2xl font-bold text-white">
-                  Halaqahs
+                  NeighborNet
               </h2>
                 <p className="mt-4 text-lg leading-8 text-gray-300">
-                  YM Halaqas are a space to chill, play, connect, and grow. Whether you come for the game, the people, or just to see what it’s about, you’ll find a community where Islam feels real, relevant, and connected to everyday life. Through activities, conversations, and brotherhood, YM helps you build friendships, strengthen your faith, and find a place where you truly belong.
-                  </p>
+                  Explain what a neighborNet is
+                   </p>
 
     </div>
        <div className="flex flex-col items-center text-center sm:items-start sm:text-left">
