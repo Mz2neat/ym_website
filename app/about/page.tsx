84453937732ab7a5b79437c71ad0160332614918
Mz2neat <syedzaidmunawar@gmail.com>
2026-06-11 10:29:24 -0400

    made changes to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,21 +3,36 @@
 import Image from "next/image";
 export default function about() {
   return (
-    <div className="min-h-screen font-sans"> {/*  can use bg-[#05070B] for actual color */}
-      <main>
-      <h1 className="ml-16 mt-10 font-bold">What is Young Muslims</h1>
-      <h1 className="text-4xl text-center tracking-tight font-extrabold mt-20 mb-1 max-w-2xl mx-auto">
+    <div className=" min-h-screen font-sans"> {/*  can use bg-[#05070B] for actual color */}
+      <section className="relative h-screen overflow-hidden">
+        <Image
+          src="/images/lexuslf4.jpg"
+          alt="BackgroundImage"
+          fill
+          className="object-cover object-center opacity-20"
+        />
+
+      <div className="absolute inset-0 bg-black/50" />
+      <main className="relative z-10">
+      <h1 className="ml-16 pt-10 font-bold text-xl">
+        What is Young Muslims
+      </h1>
+      <h1 className="text-4xl text-center tracking-tight font-extrabold mt-8 mb-1 max-w-2xl mx-auto">
       YM is not just another youth group </h1>
-      <h1 className="text-4xl text-center tracking-tight font-extrabold mt-5 max-w-2xl mx-auto">
+      <h1 className="text-4xl text-center tracking-tight font-extrabold mt-20 max-w-[85%] mx-auto">
        YM is A community where meaningful experiences, friendships, and faith naturally connect.
          {/* We should have boild statemnt here that 
-        extendt he length ofg th page  */} </h1>
-        <section className="mx-auto max-w-3xl text-center">
-          <h1 className="text-3xl tracking-tight font-bold mt-30 text-center">
+        extendt he length ofg th page  */} 
+        </h1>
+      </main>
+      
+      
+      <section className= "px-8 py-20"> 
+          <h1 className="text-3xl tracking-tight font-bold mt-45 mb-10 text-center">
             include something like what is the mission and vision of YM here. or What is YM about.
           </h1>
-          </section>
-          <div className="mx-auto mt-16 flex max-w-[85%] flex-col gap-8 md:flex-row text-center"> {/*This code segment (lines 17-41) covers the text for the mission and vission statements The width is precentage to avoid scaling issues on other devices */}
+          
+          <div className="mx-auto mt-8 flex max-w-[85%] flex-col gap-8 md:flex-row text-center"> {/*This code segment (lines 17-41) covers the text for the mission and vission statements The width is precentage to avoid scaling issues on other devices */}
             <div className="md:w-1/2 relative overflow-hidden rounded-xl border border-white/20 rounded-xl bg-white/5 p-6"> {/* makes text boxes 1/2 width of set width from above (99% of screen width)*/}
             <Image
               src="/images/3.png"
@@ -53,6 +68,9 @@ export default function about() {
             </div>
          
           </div> 
+           </section>
+           </section>
+           <main>
 
 {/*<section className="mx-auto max-w-3xl text-center">
           <h1 className="text-3xl tracking-tight font-bold mt-30 text-center">
@@ -60,24 +78,46 @@ export default function about() {
           </h1>
           </section>*/} {/*this is the what we do text/headding*/}
         
-          <div className="mt-8 mx-auto max-w-[90%] text-center"> {/* */}
-              <h2 className=" mx-auto text-2xl font-bold text-white"> {/* for halaqah heading*/}
+          <div className="mx-auto mt-8 flex max-w-[95%] flex-col gap-8 md:flex-row text-center"> {/* box for what is ym a halaqah and image of logo or halaqah */}
+               {/* LEFT SIDE: stacked text boxes */}
+            <div className="flex flex-col gap-8 md:w-1/3">
+              <div> 
+              <h2 className=" mx-auto text-lg font-bold text-white mt-16"> {/* for halaqah heading*/}
                  What is YM a Halaqah
               </h2>
-                <p className="mt-4 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto "> {/* */}
+
+                <p className="mt-8 text-md leading-8 text-gray-300"> {/* */}
          YM Halaqas are a space to chill, play, connect, and grow. Whether you come for the game, the people, or just to see what it’s about, you’ll find a community where Islam feels real, relevant, and connected to everyday life. Through activities, conversations, and brotherhood, YM helps you build friendships, strengthen your faith, and find a place where you truly belong. 
 
                 </p>
-           </div> 
-           <div className="mt-4 mx-auto max-w-[90%] text-center"> {/*what does ym a halqaah include */}
-            <h2 className=" mt-10 mx-auto text-xl font-bold text-white"> {/* */}
+              </div> 
+         <div>
+            <h2 className=" mt-16 mx-auto text-lg font-bold text-white text-center"> {/* */}
                 What does a YM Halaqah Include?
               </h2>
-                <p className="mt-2 text-lg leading-8 text-gray-300 max-w-[50%] mx-auto"> {/* */}
+                <p className="mt-8 text-md leading-8 text-gray-300 mx-auto text-center"> {/* */}
          Every YM gathering begins with a relatable Islamic reminder, a short, interactive talk led by a YM member or guest speaker, followed by an engaging activity like basketball, soccer, or gaming. It’s about learning the Deen while building brotherhood in a space where you truly belong.
                 </p>
            </div>
-<div className="mx-auto mt-16 flex max-w-5xl flex-col gap-8 md:flex-row text-center"> 
+           </div>
+           <div className="relative w-full aspect-[16/9] md:w-2/3">
+            <Image
+              src="/images/TheMangoMan.jpeg"
+              alt="YMactivity"
+              fill
+              className="object-contain rounded-lg"
+            /> 
+            </div>
+          </div>
+            <div>
+            <h2 className=" mt-10 mx-auto text-2xl font-bold text-white max-w-[60%] text-center leading-10"> {/* */}
+                The 3 majour aspects of a YM Halaqah?
+              </h2>
+                <p className="mt-2 text-md leading-8 text-gray-300 max-w-[60%] mx-auto text-center"> {/* */}
+                  </p>
+           </div>
+
+<div className="mx-auto mt-16 flex max-w-[100%] flex-col gap-8 md:flex-row text-center"> 
             <div className="md:w-1/3">
               <h2 className="text-2xl font-bold text-white">
                  Sports/Activites 
@@ -94,7 +134,7 @@ export default function about() {
               </h2>
                 <p className="mt-4 text-lg leading-8 text-gray-300">
 
-         Soemthing about inclduign Islamic reminder or talk at the beginning of the halaqah
+         Soemthing about including Islamic reminder or talk at the beginning of the halaqah
                 </p>
             </div>
       
@@ -106,16 +146,29 @@ export default function about() {
                   Explain what a neighborNet is
                    </p>
 
-    </div>
-       <div className="flex flex-col items-center text-center sm:items-start sm:text-left">
-          <h1 className="max-w-3xl text-xl font-semibold leading-10 tracking-tight text-black "> {/*This is the about page. The majour purpose is to diplsay the information about the organization and its mission. It will also include the history of the organization and its achievements. 
-            South King king AGZ once said "Kill Fudail Syed" I think its important we remember his words and follow his advice. */}
+            </div>
+          </div>
+       <div className="flex flex-col items-center text-center mt-16">
+          <h1 className="max-w-3xl text-xl font-semibold leading-10 tracking-tight text-white "> 
+           {/* This is the about page. The majour purpose is to diplsay the information about the organization and its mission. It will also include the history of the organization and its achievements. 
+            South King king AGZ once said "Kill Fudail Syed" I think its important we remember his words and follow his advice.*/}
 
             
           </h1>
       </div>
 
-      </div>
+      <div className="mx-auto mt-16 flex max-w-[95%] flex-col gap-8 md:flex-row text-center">
+            <div className="md:w-1/3">
+              <h2 className="text-2xl font-bold text-white">
+                 Real Moments Real Impact
+              </h2>
+                <p className="mt-4 text-lg leading-8 text-gray-300 max-w-[70%] mx-auto">  
+          something about halaqah ie sports etc being a space for real moments and real impact.
+          </p>
+            </div>
+            
+          </div>
+    
     </main>
 </div>
   );
